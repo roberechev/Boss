cefe60e1fa595056b7bc07f3ae77fa48f42b410f
roberechev <rober.eche.sa@gmail.com>
2019-10-25 20:32:20 +0200

    main acabado

--- src/ejercicio/Main.java
+++ src/ejercicio/Main.java
@@ -3,13 +3,23 @@ package ejercicio;
 public class Main {
 
 	public static void main(String[] args) {
-		Boss bos = new Boss();
-		Personaje gue1 = new Personaje();
-		Personaje mag = new Personaje();
-		Personaje ber = new Personaje();
+		Personaje bos = new Boss("Jose Antonio", new Ironia(""));
+		Personaje gue1 = new Guerrero("Soma", new Espada("Bastarda"));
+		Personaje mag = new Mago("Falacio", new Varita("Lobo"));
+		Personaje ber = new Berseker("Franco", new Espada("Valle"));
 
-		
-		
+		Hilo h1 = new Hilo(gue1, bos);
+		Hilo h2 = new Hilo(mag, bos);
+		Hilo h3 = new Hilo(ber, bos);
+
+		Thread th1 = new Thread(h1);
+		Thread th2 = new Thread(h2);
+		Thread th3 = new Thread(h3);
+
+		th1.start();
+		th2.start();
+		th3.start();
+
 	}
 
 }
