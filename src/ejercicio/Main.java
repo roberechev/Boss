9b2679f99c970eaa26c2a88c6499cb26a73c12d2
roberechev <rober.eche.sa@gmail.com>
2019-10-25 20:14:01 +0200

    main

--- src/ejercicio/Main.java
+++ src/ejercicio/Main.java
@@ -3,7 +3,10 @@ package ejercicio;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Boss bos = new Boss();
+		Personaje gue1 = new Personaje();
+		Personaje mag = new Personaje();
+		Personaje ber = new Personaje();
 
 	}
 
